d5a48ed96b91c4fb7476898ebb43024ff3786261
dgogo <gordelidato@gmail.com>
2023-10-26 20:51:56 +0400

    first version

---
+++ calc/Shared/functions.cs
@@ -0,0 +1,28 @@
+﻿namespace calculator_in_blazor_project.Shared;
+
+public class Functions
+{
+    public int Add(int x, int y)
+    {
+        return x+y;
+    }
+
+    public int Deduct(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+    public int Divide(int x, int y)
+    {
+        if (y<0)
+        {
+            return 0;
+        }
+
+        return x / y;
+    }
+}
